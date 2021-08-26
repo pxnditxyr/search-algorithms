585b1a8357b5b93568812be507369eb40b4ce048
Marlene <mquispel@est.emi.edu.bo>
2021-08-26 02:13:45 -0400

    Added New IDA_Star algorithm 8-puzzle

--- IDA/ida.py
+++ IDA/ida.py
@@ -35,3 +35,28 @@ def iterative_deepening_a_star_rec ( tree, heuristic, node, goal, distance, thre
 
     return min
 
+
+# graph = [
+	# [ 0, 43, 64, 0, 80, 0],
+	# [ 43, 0, 69, 81, 50, 0],
+	# [ 64, 69, 0, 94, 0, 0 ],
+	# [ 0, 81, 94, 0, 98, 157 ],
+	# [ 80, 50, 0, 98, 0, 103 ],
+	# [ 0, 0, 0, 157, 103, 0 ],
+# ]
+
+graph = [
+            [ 1, 6, 4 ],
+            [ 5, 7, 8 ],
+            [ 0, 3, 2 ],
+        ]
+
+heuristic = [
+            [ 1, 2, 3 ],
+            [ 4, 5, 6 ],
+            [ 7, 8, 0 ],
+        ]
+start = 0
+goal = 5
+
+print( graph, heuristic, start, goal )
